f71e71b9d4e17a652ad1f0918b6bd9339386f409
brasilia <ksuspec@gmail.com>
2015-06-26 22:45:49 -0300

    initial commit

---
+++ codecbase.c
@@ -0,0 +1,37 @@
+#include "codecbase.h"
+
+//Aloca memória para o dicionário
+void allocate_dictionary(struct encoder* enc, int wordMaxSize){
+    enc->dictionary = (char**) malloc( sizeof(char*) * pow(2,wordMaxSize) );
+    //Aloca memória para cada palavra do dicionário
+    int i;
+    for (i=0; i < pow(2, wordMaxSize); i++){
+        enc->dictionary[i] = (char*) malloc(sizeof(char*));
+    }
+}
+
+//Insere uma palavra no dicionário
+void insert_into_dictionary(struct encoder* enc, const char* str){
+    sprintf(enc->dictionary[enc->nextCode],"%s",str);
+    enc->nextCode++;
+}
+
+//Inicia o dicionário nos primeiros 128 índices - ASCII
+void load_ascii (struct encoder* enc){
+    //Inicia o dicionário nos primeiros 128 índices - ASCII
+    while (enc->nextCode < 128){
+        sprintf(enc->strBuffer,"%c",enc->nextCode);
+        insert_into_dictionary(enc, enc->strBuffer);
+    }
+    //Limpa o buffer
+    *(enc->strBuffer) = '\0';
+}
+
+//Prepara o codificador ou decodificador para iniciar codificação ou decodificação
+void initialize_codec(struct encoder* enc, int wordMaxSize){
+    enc->nextCode = 0;
+    enc->outBufferSize = 0;
+    enc->outBuffer = NULL;
+    allocate_dictionary(enc, wordMaxSize);
+    load_ascii(enc);
+}
---
+++ lzw.c
@@ -0,0 +1,66 @@
+/*
+    Codificador e Decodificador LZW para Compressão de Texto
+    Trabalho 1 - Multimídia - Professor Dr. Marcelo Manzato
+    Autor: Rafael Miranda Lopes - nºUSP 6520554
+*/
+
+#include "encoder.h"
+#include "decoder.h"
+
+int main (int argc, char** argv){
+    FILE* inputFile = NULL;
+    FILE* outputFile = NULL;
+    short int badArguments = 0;
+    short int codecFlag = 0; // 0 não foi escolhido, 1 codifica e -1 decodifica
+    struct encoder codec;
+
+    //Verifica número de argumentos
+    if(argc >= 6){
+        //Verifica argumentos e determina função do coder-decoder
+        if(!strcmp(argv[2], "-i") && !strcmp(argv[4], "-o")){
+            if(!strcmp(argv[1], "encode")){
+                //Encode
+                codecFlag = 1;
+                inputFile = fopen(argv[3] ,"r");
+                outputFile = fopen(argv[5], "wb");
+            }
+            else if(!strcmp(argv[1], "decode")){
+                //Decode
+                codecFlag = -1;
+                inputFile = fopen(argv[3] ,"rb");
+                outputFile = fopen(argv[5], "w");
+            }
+            else{
+                badArguments = 1;
+            }
+        }
+        else{
+            badArguments = 1;
+        }
+    }
+    else{
+        badArguments = 1;
+    }
+    //Erro: Argumentos incorretos:
+    if (badArguments || !codecFlag){
+        printf("Lista de argumentos invalida. Os argumentos devem seguir o formato:");
+        printf("\n<modo: encode ou decode> -i <arq de entrada> -o <arq de saida>\n");
+        return -1;
+    }
+    //Erro: nome do input inválido
+    if(inputFile == NULL){
+        printf("Nome de arquivo de entrada invalido.\n");
+        return -2;
+    }
+
+    //Se os parâmetros foram escolhidos corretamente:
+    if(codecFlag == 1){
+        encode(inputFile, outputFile, &codec);
+    }
+    else{
+        decode(inputFile, outputFile, &codec);
+    }
+    fclose(inputFile);
+    fclose(outputFile);
+    return 0;
+}
