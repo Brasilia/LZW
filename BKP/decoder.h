618655dacbd6bf7217da345edc0909e8b49e1019
brasilia <ksuspec@gmail.com>
2015-06-29 00:11:05 -0300

    Tirei coisa a mais

--- BKP/decoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECODER_H_INCLUDED
-#define DECODER_H_INCLUDED
-
-#include "codecbase.h"
-
-//Decodifica um arquivo binário para texto
-void decode(FILE* inFile, FILE* outFile, struct encoder* dec, short int dynamic);
-
-#endif // DECODER_H_INCLUDED
